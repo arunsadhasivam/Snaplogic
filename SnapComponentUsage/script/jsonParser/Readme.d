f992a3a4090c638b2cc89c9c466445815e2a94b0
arun <arunsadhasivam@yahoo.co.in>
2020-03-08 12:29:50 -0700

    Create Readme.d

---
+++ SnapComponentUsage/script/jsonParser/Readme.d
@@ -0,0 +1,27 @@
+input:
+=====
+
+employee-1 manages 101,102
+
+
+      "payload" : {
+        "input" : [
+          {
+            "employee_id" : "1",
+            "unique_id" : "20200306_164835",
+            "subordinates" : [
+              {
+                "employee_id" : "101",
+                "last_updated_date" : "5/29/2019 12:11",
+                "ledger_id" : "20200306_164835"
+              },
+              
+              
+               {
+                "employee_id" : "102",
+                "last_updated_date" : "5/29/2019 12:11",
+                "ledger_id" : "20200306_164835"
+              }
+            ]
+          },
+        }  
